86dbdd3d17e3e339ee6952f2e8fe576dda3fbd69
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-06 18:30:34 -0500

    updated actions but reducer can't import it, i have to debug it

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const ADD_FEATURE = 'ADD_FEATURE';
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+export const UPDATE_FEATURE = 'UPDATE_FEATURE';
+
+export const addFeature = () => ({
+    type: ADD_FEATURE,
+
+});
+
+export const removeFeature = () => ({
+    type: REMOVE_FEATURE,
+});
+
+export const updateTotal = () => ({
+    type: UPDATE_FEATURE,
+});
